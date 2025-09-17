f05c7d077a1e250cdcb81bcd88c5f524d82cd896
charlie <xyhp915@qq.com>
2025-09-17 20:57:19 +0800

    feat: add library entry point and configuration for WaveRecord

---
+++ src/lib.ts
@@ -0,0 +1,17 @@
+import '../public/style.css';
+import { EventEmitter, Timer } from './helpers.ts';
+import {
+  renderWaveform,
+  BeatsObserver,
+  RecordWaveform,
+} from './wave-record.ts';
+import { Recorder } from './recorder.ts';
+
+export {
+  EventEmitter,
+  Timer,
+  RecordWaveform,
+  renderWaveform,
+  BeatsObserver,
+  Recorder,
+};
